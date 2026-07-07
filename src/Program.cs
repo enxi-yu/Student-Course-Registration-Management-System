d48da710e5b5208d5cd573ee6b8a0bd4c0bf4f35
Coffee <3385297131@qq.com>
2026-07-07 10:19:45 +0800

    连接数据库，搭建表格

---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace StudentCourse
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // 程序启动时自动初始化数据库
+            DatabaseHelper.EnsureDatabaseInitialized();
+
+            Application.Run(new Form1());
+        }
+    }
+}
